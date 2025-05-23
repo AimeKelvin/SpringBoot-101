e742abee6cd43f52fc1832cabe2cf9e1ce2c3222
aimekelvin <kellykelvinaime@gmail.com>
2025-05-24 00:14:18 +0200

    init files

---
+++ src/test/java/me/shimwa/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.shimwa.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
